d98810162f11827970f2b38b020536d7feef7360
polidorl <polidor.lisbeth4@gmail.com>
2024-06-23 16:23:27 -0400

    adding graphics with their names

--- app/charts.py
+++ app/charts.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 def generate_bar_chart(name, labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
+  #plt.savefig('bar.png')
   plt.savefig(f'./imgs/{name}.png')
   plt.close()
 
--- app/main.py
+++ app/main.py
@@ -1,29 +1,15 @@
 import utils
 import read_csv
 import charts
-import pandas as pd
 
 def run():
-  '''
   data = read_csv.read_csv('data.csv')
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
-
-  '''
-  #con las sig 4 lineas  reemplazan las lineas11 y 12 para generar pie_chart pero usando Pandas
-  df = pd.read_csv('data.csv')
-  df = df[df['Continente'] == 'Africa']
-  #df = df[df['Continente'] == 'South America']
   
-  countries = df['Country'].values
-  percentages = df['World Population Percentage'].values
-  
-  charts.generate_pie_chart(countries, percentages)
-  
-  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
   print(country)
 
@@ -33,8 +19,7 @@ def run():
     country = result[0]
     print(country)
     labels, values = utils.get_population(country)
+    #charts.generate_bar_chart(labels, values)
     charts.generate_bar_chart(country['Country'], labels, values)
-
-
 if __name__ == '__main__':
   run()
